5ace9ff0fd01083b9be6f2b44b83ed7956191ff3
RianKhanafi <riankhanafi69@gmail.com>
2023-03-26 09:06:13 +0700

    update context

--- src/context/context.ts
+++ src/context/context.ts
@@ -1,10 +1,11 @@
+"use client";
 import { IUser } from "module/users/hoox";
 import React from "react";
-
-export const ContextProvider = React.createContext<{
+export interface IContext {
   users: IUser | null;
   updateData: (e: IUser) => void;
-}>({
+}
+export const ContextProvider = React.createContext<IContext>({
   users: null,
   updateData: () => {},
 });
